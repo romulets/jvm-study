10179684c6915343bf9b6fce43cc2ff191fbb367
Rômulo Farias <romulo.farias@elastic.co>
2025-08-03 20:51:19 +0200

    Add binary search

--- src/datastructures/BinarySearch.java
+++ src/datastructures/BinarySearch.java
@@ -1,2 +1,64 @@
-package datastructures;public class BinarySearch {
+package datastructures;
+
+public class BinarySearch {
+
+    /**
+     * O(log(n))
+     * @param array
+     * @param value
+     * @return index of value. -1 if not found
+     */
+    public static <T extends Comparable<T>> int search(DoubleEdgedLinearArray<T> array, T value) {
+        int begin = 0;
+        int end = array.size() - 1;
+
+        while (end >= begin) {
+            int half = Math.ceilDiv(end - begin, 2) + begin;
+
+            int compare = array.at(half).compareTo(value);
+            if (compare == 0) { // found
+                return half;
+            }
+
+            // half is larger that value,
+            // thus the result like is at first half
+            if (compare > 0) {
+                end = half - 1;
+            } else {
+                begin = half + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * O(log(n))
+     * @param array
+     * @param value
+     * @return index of value. -1 if not found
+     */
+    public static <T extends Comparable<T>> int search(T[] array, T value) {
+        int begin = 0;
+        int end = array.length - 1;
+
+        while (end >= begin) {
+            int half = Math.ceilDiv(end - begin, 2) + begin;
+
+            int compare = array[half].compareTo(value);
+            if (compare == 0) { // found
+                return half;
+            }
+
+            // half is larger that value,
+            // thus the result like is at first half
+            if (compare > 0) {
+                end = half - 1;
+            } else {
+                begin = half + 1;
+            }
+        }
+
+        return -1;
+    }
 }
---
+++ tests/datastructures/BinarySearchTest.java
@@ -0,0 +1,93 @@
+package datastructures;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTest {
+
+    @Test
+    void search_half() {
+        Integer[] array = new Integer[]{1, 2 ,3};
+
+        assertEquals(1, BinarySearch.search(array, 2));
+    }
+    @Test
+    void search_edges() {
+        Integer[] array = new Integer[]{1, 2 ,3};
+
+        assertEquals(2, BinarySearch.search(array, 3));
+        assertEquals(0, BinarySearch.search(array, 1));
+    }
+
+    @Test
+    void search_even() {
+        Integer[] array = new Integer[]{1, 2 ,3, 4};
+
+        assertEquals(0, BinarySearch.search(array, 1));
+        assertEquals(1, BinarySearch.search(array, 2));
+        assertEquals(2, BinarySearch.search(array, 3));
+        assertEquals(3, BinarySearch.search(array, 4));
+    }
+
+    @Test
+    void search_two() {
+        Integer[] array = new Integer[]{1, 2};
+
+        assertEquals(0, BinarySearch.search(array, 1));
+        assertEquals(1, BinarySearch.search(array, 2));
+    }
+
+    @Test
+    void search_one() {
+        Integer[] array = new Integer[]{1};
+
+        assertEquals(0, BinarySearch.search(array, 1));
+    }
+
+    @ParameterizedTest()
+    @MethodSource("sortedArgumentProvider")
+    public void search_many(Integer[] input) {
+        for (int i = 0; i < input.length - 1; i++) {
+            assertEquals(i, BinarySearch.search(input, i));
+        }
+    }
+
+    @ParameterizedTest()
+    @MethodSource("sortedArgumentProvider")
+    public void search_doubleEdgedLinearArray_many(Integer[] input) {
+        DoubleEdgedLinearArray<Integer> array = new DoubleEdgedLinearArray<>(input.length * 2);
+        for (Integer i : input) {
+            array.insertLast(i);
+        }
+
+        for (int i = 0; i < array.size() - 1; i++) {
+            assertEquals(i, BinarySearch.search(array, i));
+        }
+    }
+
+
+    static Stream<Arguments> sortedArgumentProvider() {
+        int times = 1000;
+        Arguments[] arguments = new Arguments[times];
+        for (int  i = 0; i < times; i++) {
+            arguments[i] = Arguments.of((Object) generateSortedArray(i + 1));
+        }
+
+        return Arrays.stream(arguments);
+    }
+    static Integer[] generateSortedArray(int size) {
+        Integer[] array = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+}
